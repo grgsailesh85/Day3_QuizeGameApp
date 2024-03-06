89e616238550a0b56e3be060ceecb45b6860ca77
grgsailesh85 <143858404+grgsailesh85@users.noreply.github.com>
2024-03-06 20:15:39 +0545

    not completed

---
+++ JavaScript/index.js
@@ -0,0 +1,38 @@
+const questions = [
+  {
+    question : "Which is the largest animal in the world?",
+    answers : [
+      {Text:"Shark", correct: false},
+      {Text:"Blue Whale", correct: true},
+      {Text:"Elephant", correct: false},
+      {Text:"Giraffe", correct: false},
+    ]
+  },
+  {
+    question : "Which is the smallest country in the world?",
+    answers : [
+      {Text:"Vatican City", correct: true},
+      {Text:"Bhutan", correct: false},
+      {Text:"Nepal", correct: false},
+      {Text:"Sri Lanka", correct: false},
+    ]
+  },
+  {
+    question : "Which is the largest desert in the world?",
+    answers : [
+      {Text:"Kalahari", correct: false},
+      {Text:"Gobi", correct: false},
+      {Text:"Sahara", correct: false},
+      {Text:"Antarctica", correct: true},
+    ]
+  },
+  {
+    question : "Which is the smallest continent in the world?",
+    answers : [
+      {Text:"Asia", correct: false},
+      {Text:"Australia", correct: true},
+      {Text:"Arctic", correct: false},
+      {Text:"Africa", correct: false},
+    ]
+  },
+];
